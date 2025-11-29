5100aa41891d42a0b6a1a929c9c2e6647d01d136
Kasra Fouladi <152831297+kasrafouladi@users.noreply.github.com>
2025-11-29 13:30:05 +0330

    edit

--- elmosyar_back/posts/serializers.py
+++ elmosyar_back/posts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from accounts.serializers import UserSerializer
-from .models import Post, PostMedia
+from .models import Post, PostMedia, CategoryFormat
 
 
 class PostMediaSerializer(serializers.ModelSerializer):
