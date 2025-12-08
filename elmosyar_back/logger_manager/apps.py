f83cebdfe963de82006a617d1ceabda8f3b56415
Kasra Fouladi <152831297+kasrafouladi@users.noreply.github.com>
2025-12-08 19:56:43 +0330

    .

--- elmosyar_back/logger_manager/apps.py
+++ elmosyar_back/logger_manager/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 class LogManagerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'log_manager'
+    name = 'logger_manager'
     verbose_name = 'مدیریت لاگ‌ها'
